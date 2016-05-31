4611d3c58dadea32bf5b2e33dffa376e2c16a4da
chandrasekhara_reddy <chandrasekhara_reddy@persistent.co.in>
2016-05-31 17:52:28 +0530

    My First Program

---
+++ src/com/psl/guru99/Login.java
@@ -0,0 +1,34 @@
+package com.psl.guru99;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	
+	
+	@Test
+	public void login()
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.demo.guru99.com/V4/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("uid")).sendKeys("mngr38718");
+		driver.findElement(By.name("password")).sendKeys("yhuhurE");
+		driver.findElement(By.name("btnLogin")).click();
+		String LoginStatus = driver.findElement(By.xpath("html/body/table/tbody/tr/td/table/tbody/tr[3]/td")).getText();
+		System.out.println(LoginStatus);
+		String Title = driver.getTitle();
+		System.out.println(Title);
+		
+		//String ActualStatus = "Manager Id : mngr38718";
+				
+		Assert.assertEquals("Manger Id : mngr38718", LoginStatus, "Login is not Successfull");
+		driver.quit();
+		
+	}
+
+}
